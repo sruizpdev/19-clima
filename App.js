7bed7cb44f9beafdff6eed97953ae28196b61462
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-10-19 10:23:13 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>hola</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ components/Formulario.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Formulario = () => {
+  return <Text>Desde formulario</Text>;
+};
+
+export default Formulario;
